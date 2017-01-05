63eb28d6b9c303deded819d127987103f72e8ed5
undefz <undefz@gmail.com>
2017-01-05 23:28:15 +0400

    added db schema

---
+++ db/db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `pomo_history` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `started` datetime DEFAULT NULL,
+  `ended` datetime DEFAULT NULL,
+  `finished` smallint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
